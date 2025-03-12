4a3f56b3e13bb43358ee28ef14a347b8bf068493
Johab Freitas <freitasjohab@gmail.com>
2025-03-11 22:14:06 -0300

    Anotações JPA para classes de domínio

--- src/main/java/br/com/johabfreitas/helpdesk/domain/Pessoa.java
+++ src/main/java/br/com/johabfreitas/helpdesk/domain/Pessoa.java
@@ -1,24 +1,51 @@
 package br.com.johabfreitas.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.johabfreitas.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-public abstract class Pessoa {
+@Entity(name = "TB_PESSOA")
+public abstract class Pessoa implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
+	
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
+	
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER) //Estou assegurando que a minha lista de perfis venha junto com o usuário
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
-	Pessoa(){
+	public Pessoa(){
 		addPerfil(Perfil.CLIENTE);
 	}
 
